8b7b6aa0e40355e1cbd1f0b57ee1a6f700f07f80
Devin Shanesha <devin.shanesha@gmail.com>
2024-11-06 00:59:14 +0530

    Solve Anagram Question

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
-
+    // Check if lengths are different
+    if (str1.length !== str2.length) {
+      return false;
+    }
+  
+    // Sort both strings and compare
+    let sortedStr1 = str1.toLowerCase().split('').sort().join('');
+    let sortedStr2 = str2.toLowerCase().split('').sort().join('');
+  
+    return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
