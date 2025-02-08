74a65035aea03bdcae8587df814b1a5ea5f21785
Devin Shanesha <devin.shanesha@gmail.com>
2025-02-09 01:15:05 +0530

    solve few questions

---
+++ week-2/01-async-js/easy/1-counter.js
@@ -0,0 +1,15 @@
+function couter(){
+    let count = 0;
+    setInterval(()=>{
+        console.log(count);
+        count++;
+    }, 1000)
+}
+
+couter();
+
+// let count = 0;
+// setInterval(()=>{
+//     console.log(count);
+//     count++;
+// }, 1000)
